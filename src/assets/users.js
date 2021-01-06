5c6150d532ea5653978a1a41327c179c82f62b6c
sandeepraj <sandeepraj.tandon@gov.bc.ca>
2021-01-05 16:27:55 -0800

    Chapter 9 done

--- src/assets/users.js
+++ src/assets/users.js
@@ -5,35 +5,63 @@ export const users = [
             firstName: 'Sandeepraj',
             lastName: 'Singh',
             email: 'sandeepraj.tandon@cgi.com',
-            followers: 0,
+            followers: 10,
             twoots: [
-                {id: 1, content:'Welcome to my twooter profile'},
-                {id: 2, content:'I am blah blah and blah and passionate about plah plah and plah!'}
+                {id: 1, content:'Husband, Father, Full Stacker'},
+                {id: 2, content:'My mother says i am intelligent and handsome but my Wife just knows me far too much to pretend its true!'}
             ]
             
         },
         {
             id:2,
-            username:'kaurLovely',
-            firstName: 'Lovely',
-            lastName: 'Kaur',
-            email: 'lovely.schoolhouseteaching@shaw.ca',
-            followers: 0,
+            username:'signoriAndrew',
+            firstName: 'Andrew',
+            lastName: 'Signori',
+            email: 'andrew.signori@email.com',
+            followers: 50,
             twoots: [
-                {id: 1, content:'Welcome to School House Teaching Supplies!'},
-                {id: 2, content:'It is an educational store for Everyone!!'}
+                {id: 1, content:'I am a century old developer on Advanced Education!'},
+                {id: 2, content:'I will remain a developer for ever on Advanced Education!!'}
             ]
             
         },
         {
             id:3,
-            username:'guddikaur',
-            firstName: 'guddi',
-            lastName: 'kaur',
-            email: 'guddikaur@gmail.com',
-            followers: 0,
+            username:'mitraPushan',
+            firstName: 'Pushan',
+            lastName: 'Mitra',
+            email: 'pushan.mitra@email.com',
+            followers: 40,
             twoots: [
-              
+                {id: 1, content:'I for sure know about pipelines!'},
+                              
+            ]
+            
+        },
+        {
+            id:4,
+            username:'schwenkarAndrew',
+            firstName: 'Andrew',
+            lastName: 'schwenkar',
+            email: 'andrew.schwenkar@email.com',
+            followers: 30,
+            twoots: [
+                {id: 1, content:'To Camanda or not to Camanda'},
+                              
+            ]
+            
+        },
+        {
+            id:5,
+            username:'smithMichelle',
+            firstName: 'Michelle',
+            lastName: 'Smith',
+            email: 'michelle.smith@email.com',
+            followers: 3000,
+            twoots: [
+                {id: 1, content:'Advanced Education needs me as Scrum Master, Project Manager and a glue!!'},
+                {id: 2, content:'I will have a blue tick soon!'},
+                              
             ]
             
         }
